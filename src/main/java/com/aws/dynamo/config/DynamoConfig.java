d27cc64f0b38e61ecf8484460446576a0d2bb548
João Marcos <joaomarcossm2013@hotmail.com>
2026-06-25 21:53:09 -0300

    criando dynamoDB

--- src/main/java/com/aws/dynamo/config/DynamoConfig.java
+++ src/main/java/com/aws/dynamo/config/DynamoConfig.java
@@ -1,21 +1,21 @@
-package com.aws.s3.config;
+package com.aws.dynamo.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 @Configuration
-public class S3Config {
+public class DynamoConfig {
 
     @Value("${aws.dynamodb.region}")
     private String region;
 
     @Bean
-    public S3Client s3Client() {
-        return S3Client.builder()
+    public DynamoDbClient dynamoDbClient() {
+        return DynamoDbClient.builder()
                 .region(Region.of(region))
                 .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
